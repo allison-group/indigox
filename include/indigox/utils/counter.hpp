1521ca4470707903ae05c75fe54ef37945faaf63
Welsh <i.welsh@massey.ac.nz>
2018-04-16 11:20:06 +1200

    Add GetCurrentCount method to CountableObject
    Documentation cleanup

--- include/indigox/utils/counter.hpp
+++ include/indigox/utils/counter.hpp
@@ -1,51 +1,45 @@
-/** @file counter.hpp
- *  @brief Declaration of the CountableObject template.
- *  @author Ivan Welsh
- *  @date 11 November 2017
- *  @lastmodify 6 January 2018
- *  @version 0.1
- *  @copyright The MIT License
- */
+/*! \file counter.hpp */
 
 #ifndef INDIGOX_UTILS_COUNTER_HPP
 #define INDIGOX_UTILS_COUNTER_HPP
 
-#include <cstdint>
+#include "numerics.hpp"
 
-namespace indigox {
-  namespace utils {
+namespace indigox::utils {
+  
+  /*! \class CountableObject counter.hpp indigox/utils/counter.hpp
+   *  \brief Template class for counting instances.
+   *  \tparam T class requiring counting.
+   *  \details Provides a means to count number of instances of a type created.
+   *  Classes should derive from this template and call the constructor in class
+   *  initalisation list. */
+  template <class T>
+  class CountableObject {
     
-    /** @class CountableObject counter.hpp utils/counter.hpp
-     *  @brief Template class for counting instances.
-     *  @tparam T class requiring counting.
-     *  @details Provides a means to count instances of an object. Classes
-     *  should derive from this template and call the constructor in class
-     *  initalisation list.
-     *  @since 0.1
+  public:
+    /*! \brief Default constructor.
+     *  \details Sets the unique id associated with the class instance.
      */
-    template <class T>
-    class CountableObject {
-      
-    public:
-      /** @brief Default constructor.
-       *  @details Sets the unique id associated with the class instance.
-       */
-      CountableObject() : id_(++__id_generator_) { }
-      
-      /** @brief Obtain the class instance unique id.
-       *  @returns the unique id.
-       */
-      inline uid_t GetUniqueID() const { return id_; }
-      
-    private:
-      const uid_t id_;
-      static uid_t __id_generator_;
-    };
+    CountableObject() : _id(++_count) { }
     
-    template <class T>
-    uid_t CountableObject<T>::__id_generator_ = uid_t(-1);
+    /*! \brief Obtain the class instance unique id.
+     *  \returns the unique id. */
+    inline uid_ GetUniqueID() const { return _id; }
     
-  }
+    /*! \brief Get the current instance count.
+     *  \return the current number of created instances of the type. */
+    static uid_ GetCurrentCount() { return _count; }
+    
+  private:
+    //! \brief Unique ID.
+    const uid_ _id;
+    //! \brief Count.
+    static uid_ _count;
+  };
+  
+  template <class T>
+  uid_ CountableObject<T>::_count = 0;
+  
 }
 
 #endif /* INDIGOX_UTILS_COUNTER_HPP */
