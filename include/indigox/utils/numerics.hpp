729fceded72163db6315096e40364b831a8b91f6
Welsh <i.welsh@massey.ac.nz>
2018-04-16 11:14:36 +1200

    Added numerics.hpp for typedefs of numeric types

---
+++ include/indigox/utils/numerics.hpp
@@ -0,0 +1,23 @@
+#ifndef INDIGOX_UTILS_NUMERICS_HPP
+#define INDIGOX_UTILS_NUMERICS_HPP
+
+#include <cstdint>
+#include <string>
+
+namespace indigox {
+  typedef int8_t char_;
+  typedef uint8_t uchar_;
+  typedef int16_t short_;
+  typedef uint16_t ushort_;
+  typedef int32_t int_;
+  typedef uint32_t uint_;
+  typedef int64_t long_;
+  typedef uint64_t ulong_;
+  typedef double float_;
+  typedef size_t size_;
+  typedef uint64_t uid_;
+  
+  typedef std::string string_;
+} // namespace indigox
+
+#endif /* INDIGOX_UTILS_NUMERICS_HPP */
