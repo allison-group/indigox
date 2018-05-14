73cc50bf01b62a8fde8c621aca077b73c19edd94
Welsh <i.welsh@massey.ac.nz>
2018-05-14 14:52:31 +1200

    Documentation and cleanup for utility functions

--- include/indigox/utils/common.hpp
+++ include/indigox/utils/common.hpp
@@ -1,32 +1,58 @@
-/** @file common.hpp
- *  @brief Utility functions declarations.
- *  @author Ivan Welsh
- *  @date 21 August 2017
- *  @lastmodify 6 January 2018
- *  @version 0.1
- *  @copyright The MIT License
+/*! \file common.hpp
  */
 
 #ifndef INDIGOX_UTILS_COMMON_HPP
 #define INDIGOX_UTILS_COMMON_HPP
 
-#include <string>
+#include <type_traits>
 
+#include "numerics.hpp"
+
+//! utils namespace for useful utility functions
 namespace indigox::utils {
   
-  /// @brief Convert a string to upper case.
-  std::string toUpper(const std::string*);
+  /*! \brief Convert a string to upper case.
+   *  \param s the string to convert.
+   *  \return the uppercase version of s. */
+  string_ ToUpper(const string_& s);
   
-  /// @brief Convert a string to lower case.
-  std::string toLower(const std::string*);
+  /*! \brief Convert a string to lower case.
+   *  \param s the string to convert.
+   *  \return the lower case version of s. */
+  string_ ToLower(const string_& s);
   
-  /// @brief Convert a string to lower case with leading upper case.
-  std::string toUpperFirst(const std::string*);
+  /*! \brief Convert a string to lower case with a single leading upper case.
+   *  \details Acts on the string as a whole, ignoring any white space.
+   *  \param s the string the convert.
+   *  \return the lower case version of s with a leading upper case letter. */
+  string_ ToUpperFirst(const string_& s);
   
-  /// @brief Generate a random string.
-  std::string randomString(size_t,
-                           const std::string chrs = "qwertyuiopasdfghjklzxcvbnmZAQXSWCDEVFRBGTNHYMJUKILOP");
-}  // namespace indigox
+  /*! \brief Generate a random string.
+   *  \details All upper and lower case letters are available.
+   *  \param length the number of characters to generate.
+   *  \param seed the seed for the random number generator. If 
+   *  \return the randomly generated string. */
+  string_ GetRandomString(size_ length, size_ seed = 0);
+  
+  /*! \brief Check if a given shared_ptr<T> is in a weak_ptr<T> container.
+   *  \details Checks all weak_ptr<T> instances in the iterator range to see
+   *  if the reference the supplied shared_ptr<T>. If the supplied shared_ptr<T>
+   *  is empty, no comparisons are made.
+   *  \tparam T the element_type.
+   *  \tparam __Iter the iterator type.
+   *  \param b,e start and end of the iterator range.
+   *  \param x the shared_ptr<T> instance to search for.
+   *  \return the iterator position of the first found element. */
+  template<typename T, typename __Iter>
+  inline __Iter WeakContainsShared(__Iter b, __Iter e, std::shared_ptr<T> x) {
+    static_assert(std::is_same<
+                  typename std::weak_ptr<T>,
+                  typename std::iterator_traits<__Iter>::value_type>::value,
+                  "__Iter must be iterator over std::weak_ptr<T>.");
+    if (!x) return e;
+    return std::find_if(b, e, [x](std::weak_ptr<T> _x) { return _x.lock() == x; });
+  }
+}  // namespace indigox::utils
 
 
 #endif /* INDIGOX_UTILS_COMMON_HPP */
--- src/utils/common.cpp
+++ src/utils/common.cpp
@@ -1,65 +1,42 @@
-/** @file common.cpp
- *  @brief Utility functions implementation.
- *  @author Ivan Welsh
- *  @date 21 August 2017
- *  @lastmodify 6 January 2018
- *  @version 0.1
- *  @copyright The MIT License
- */
-
 #include <algorithm>
 #include <random>
-#include <string>
 
-#include "indigox/utils/common.hpp"
+#include <indigox/utils/common.hpp>
+#include <indigox/utils/numerics.hpp>
 
-//! utils namespace for some useful utilities
 namespace indigox::utils {
+  
+  string_ ToUpper(const string_& s){
+    string_ t = s;
+    std::transform(t.begin(), t.end(), t.begin(), ::toupper);
+    return t;
+  }
+  
+  string_ ToLower(const string_& s){
+    string_ t = s;
+    std::transform(t.begin(), t.end(), t.begin(), ::tolower);
+    return t;
+  }
+  
+  string_ ToUpperFirst(const string_& s){
+    string_ t = s;
+    std::transform(t.begin(), t.begin() + 1, t.begin(), ::toupper);
+    std::transform(t.begin() + 1, t.end(), t.begin() + 1, ::tolower);
+    return t;
+  }
+  
+  string_ GetRandomString(size_ length, size_ seed) {
+    static string_ chrs = "qwertyuiopasdfghjklzxcvbnmZAQXSWCDEVFRBGTNHYMJUKILOP";
+    static std::mt19937 rg{std::random_device{}()};
+    static std::uniform_int_distribution<size_t> pick(0, chrs.size() - 1);
+    if (seed != 0) rg.seed(seed);
     
-    /** @param s the string to convert.
-     *  @returns the upper case string.
-     */
-  std::string toUpper(const std::string* s){
-      std::string t = *s;
-      std::transform(t.begin(), t.end(), t.begin(), ::toupper);
-      return t;
-    }
+    string_ s;
+    s.reserve(length + 4);  // no need to copy when adding extensions
     
-    /** @param s the string to convert.
-     *  @returns the lower case string.
-     */
-    std::string toLower(const std::string* s){
-      std::string t = *s;
-      std::transform(t.begin(), t.end(), t.begin(), ::tolower);
-      return t;
-    }
-    
-    /** @details Assumes the string is a single word.
-     *  @param s the string to convert.
-     *  @returns the lower case string with leading upper case.
-     */
-    std::string toUpperFirst(const std::string* s){
-      std::string t = *s;
-      std::transform(t.begin(), t.begin() + 1, t.begin(), ::toupper);
-      std::transform(t.begin() + 1, t.end(), t.begin() + 1, ::tolower);
-      return t;
-    }
-    
-    /** @details Characters available are all upper and lower case letters.
-     *  @param length the number of characters in the generated string.
-     *  @returns a randomly generated string.
-     */
-  std::string randomString(size_t length, const std::string chrs) {
-      static std::mt19937 rg{std::random_device{}()};
-      static std::uniform_int_distribution<size_t> pick(0, sizeof(chrs) - 2);
-      
-      std::string s;
-      s.reserve(length + 4);  // no need to copy when adding extensions
-      
-      while(length--)
-        s += chrs[pick(rg)];
-      return s;
-      
-    }
-    
+    while(length--)
+      s += chrs[pick(rg)];
+    return s;
+  }
+  
 }  // namespace indigox::utils
