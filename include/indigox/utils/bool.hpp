f96e93048c951eec6d30ac7260032f65f0a73fe2
Welsh <i.welsh@massey.ac.nz>
2018-05-16 09:37:01 +1200

    Docstrings for IXBool

--- include/indigox/utils/bool.hpp
+++ include/indigox/utils/bool.hpp
@@ -2,11 +2,17 @@
 #define INDIGOX_UTILS_BOOL_HPP
 
 #include <string>
+
 namespace indigox::utils {
+  /*! \brief A boolean class for indicating a reason for the boolean state. */
   struct IXBool {
+    //! \brief The boolean state.
     const bool success;
+    //! \brief The reason for the state.
     const std::string reason;
+    //! \brief Default constructor.
     IXBool(bool p, std::string r) : success(p), reason(r) { }
+    //! \brief Implicit conversion to the primitive bool type.
     operator bool() const { return success; }
   };
 }
