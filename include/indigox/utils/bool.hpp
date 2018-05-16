619d7a811079c5e2a15b70834641d5f7188933f7
Welsh <i.welsh@massey.ac.nz>
2018-05-17 09:11:15 +1200

    IXBool improvements
    Changed reason to be enum class to enable easier comparison for fail reasons.
    Added operator== for comparisons with Reasons

--- include/indigox/utils/bool.hpp
+++ include/indigox/utils/bool.hpp
@@ -6,16 +6,42 @@
 namespace indigox::utils {
   /*! \brief A boolean class for indicating a reason for the boolean state. */
   struct IXBool {
+    enum class Reason {
+      // General fails
+      BAD_REQUEST,
+      // Graph fails
+      NO_DUPLICATES,
+      NO_SELF_LOOPS,
+      NO_PARALLEL_EDGES,
+      MISSING_EDGE,
+      // Pass
+      SUCCESSFUL
+    };
+    
     //! \brief The boolean state.
-    const bool success;
+    bool success;
     //! \brief The reason for the state.
-    const std::string reason;
+    Reason reason;
     //! \brief Successful constructor
-    IXBool() : success(true), reason("Successful") { }
+    IXBool() : success(true), reason(Reason::SUCCESSFUL) { }
     //! \brief Failed constructor.
-    IXBool(std::string r) : success(false), reason(r) { }
+    IXBool(Reason r) : success(false), reason(r) { }
     //! \brief Implicit conversion to the primitive bool type.
     operator bool() const { return success; }
+    //! \brief Assignment operator.
+    IXBool& operator=(const IXBool& o) {
+      if (&o == this) return *this;
+      success = o.success; reason = o.reason;
+      return *this;
+    }
+    //! \brief Compare an IXBool with a Reason
+    inline friend bool operator==(const IXBool& b, IXBool::Reason r) {
+      return b.reason == r;
+    }
+    //! \brief Compare a Reason with an IXBool
+    inline friend bool operator==(IXBool::Reason r, const IXBool& b) {
+      return b.reason == r;
+    }
   };
 }
 
