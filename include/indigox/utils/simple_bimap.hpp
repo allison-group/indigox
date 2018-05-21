b89a8a660a45d781baa1a83c507162e066b02156
Welsh <i.welsh@massey.ac.nz>
2018-05-22 09:10:09 +1200

    SimpleBiMap
    A simple bidirectional map which allows mapping key <-> value and vice versa.

---
+++ include/indigox/utils/simple_bimap.hpp
@@ -0,0 +1,14 @@
+//
+//  simple_bimap.hpp
+//  indigox_test
+//
+//  Created by Welsh, Ivan on 22/05/18.
+//  Copyright © 2018 Allison Group. All rights reserved.
+//
+
+#ifndef simple_bimap_hpp
+#define simple_bimap_hpp
+
+#include <stdio.h>
+
+#endif /* simple_bimap_hpp */
