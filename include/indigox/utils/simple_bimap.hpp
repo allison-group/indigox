e0adb43e8573e0e056a8c0444efd7dadaec9d89f
iwelsh47 <8754457+iwelsh47@users.noreply.github.com>
2018-07-13 09:27:54 +1200

    Make SimpleBiMap use EASTL::vector_map instead of std::map

--- include/indigox/utils/simple_bimap.hpp
+++ include/indigox/utils/simple_bimap.hpp
@@ -1,4 +1,4 @@
-#include <map>
+#include <EASTL/vector_map.h>
 
 #ifndef INDIGOX_UTILS_SIMPLE_BIMAP_HPP
 #define INDIGOX_UTILS_SIMPLE_BIMAP_HPP
@@ -6,8 +6,8 @@
 namespace indigox::utils {
   template <class L, class R>
   struct SimpleBiMap {
-    std::map<L, R> left;
-    std::map<R, L> right;
+    eastl::vector_map<L, R> left;
+    eastl::vector_map<R, L> right;
     
     inline void insert(L l, R r) {
       left.emplace(l, r);
