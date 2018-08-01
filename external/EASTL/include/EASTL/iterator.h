f0033893961ec043f1f2ddf7f0682128b77fd01a
iwelsh47 <8754457+iwelsh47@users.noreply.github.com>
2018-08-02 09:24:52 +1200

    Fixed some of the internals of eastl not compiling with the default EASTL_STD_ITERATOR_CATEGORY_ENABLED being 1 instead of 0

--- external/EASTL/include/EASTL/iterator.h
+++ external/EASTL/include/EASTL/iterator.h
@@ -78,14 +78,14 @@ namespace eastl
 	// Iterator categories
 	// Every iterator is defined as belonging to one of the iterator categories that
 	// we define here. These categories come directly from the C++ standard.
-	#if !EASTL_STD_ITERATOR_CATEGORY_ENABLED // If we are to use our own iterator category definitions...
+//	#if !EASTL_STD_ITERATOR_CATEGORY_ENABLED // If we are to use our own iterator category definitions...
 		struct input_iterator_tag { };
 		struct output_iterator_tag { };
 		struct forward_iterator_tag       : public input_iterator_tag { };
 		struct bidirectional_iterator_tag : public forward_iterator_tag { };
 		struct random_access_iterator_tag : public bidirectional_iterator_tag { };
 		struct contiguous_iterator_tag    : public random_access_iterator_tag { };  // Extension to the C++ standard. Contiguous ranges are more than random access, they are physically contiguous.
-	#endif
+//	#endif
 
 
 	// struct iterator
