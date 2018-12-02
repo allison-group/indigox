7cbed7dba954f95674767e4108c71c7ef7d7956c
iwelsh47 <8754457+iwelsh47@users.noreply.github.com>
2018-12-03 11:42:28 +1300

    Updated .clang-format. Angle pimpl-ise in cpp file and headers lightened.

--- include/indigox/classes/angle.hpp
+++ include/indigox/classes/angle.hpp
@@ -1,13 +1,9 @@
 /*! \file angle.hpp */
+
+#include "../utils/fwd_declares.hpp"
+
 #include <array>
 #include <memory>
-#include <string>
-
-#include "forcefield.hpp"
-#include "../utils/common.hpp"
-#include "../utils/fwd_declares.hpp"
-#include "../utils/counter.hpp"
-#include "../utils/triple.hpp"
 
 #ifndef INDIGOX_CLASSES_ANGLE_HPP
 #define INDIGOX_CLASSES_ANGLE_HPP
@@ -18,94 +14,76 @@ namespace indigox {
     friend class indigox::Molecule;
     //! \brief Friendship allows serialisation.
     friend class cereal::access;
-    
+
   public:
     //! \brief Container for storing Atom reference assigned to an Angle.
     using AngleAtoms = std::array<Atom, 3>;
-    
+
   private:
     template <typename Archive>
-    void serialise(Archive& archive, const uint32_t version);
-    
+    void serialise(Archive &archive, const uint32_t version);
+
   public:
     INDIGOX_GENERIC_PIMPL_CLASS_DEFAULTS(Angle);
     INDIGOX_GENERIC_PIMPL_CLASS_OPERATORS(Angle, ang);
-    
+
   private:
     /*! \brief Normal constructor.
      *  \details Creates an angle between three atoms, linking it to the given
      *  Molecule. Atom \p b is always the central atom of the angle.
      *  \param a,b,c the three atoms to construct an angle between.
      *  \param m the molecule to assign the angle to. */
-    Angle(const Atom& a, const Atom& b, const Atom& c, const Molecule& m);
-    
+    Angle(const Atom &a, const Atom &b, const Atom &c, const Molecule &m);
+
   public:
+    /*! \brief Check if the angle has associated parameters.
+     *  \return if there is a valid parameter. */
+    bool HasType() const;
+
+    /*! \brief Number of atoms this angle contains.
+     *  \return 3. */
+    constexpr int64_t NumAtoms() const {
+      return 3;
+    }
+
     /*! \brief Tag of the angle.
      *  \details This value may be modified without warning. Use with caution.
      *  For a constant identifier to the angle, use the Angle::GetUniqueID()
      *  method.
      *  \return the tag assigned to the angle. */
-    int32_t GetTag() const;
-    
+    int64_t GetTag() const;
+
+    int64_t GetID() const;
+
     /*! \brief Molecule this angle is associated with.
      *  \details The returned shared_ptr is empty if the angle is not assigned
      *  to a valid molecule.
      *  \return the molecule associated with this angle. */
-    const Molecule& GetMolecule() const;
-    
-    /*! \brief String representation of the angle.
-     *  \details If any of the atoms of the angle are not valid, the returned
-     *  string is Angle(MALFORMED), otherwise it is of the form:
-     *  Angle(NAME_BEGIN, NAME_CENTRAL, NAME_END).
-     *  \return a string representation of the dihedral. */
-    std::string ToString() const;
-    
-    /*! \brief Set the tag of this angle.
-     *  \details The tag of an angle should not be considered stable. Use with
-     *  caution.
-     *  \param tag the tag to set. */
-    void SetTag(int32_t tag);
-    
+    const Molecule &GetMolecule() const;
+
     /*! \brief Get the atoms of the angle.
-     *  \return triple of the atoms of the angle. */
-    const AngleAtoms& GetAtoms() const;
-    
-    /*! \brief Number of atoms this angle contains.
-     *  \return 3. */
-    constexpr int64_t NumAtoms() const { return  3; }
-    
+     *  \return the atoms of the angle. */
+    const AngleAtoms &GetAtoms() const;
+
     /*! \brief Get the index from the molecule.
      *  \details Calculates the index of the angle in the container of angles
      *  of the molecule it is a part of. If the angle is not a real angle, eg
      *  empty data variable, the returned value is -1.
      *  \return the index of the angle. */
     int64_t GetIndex() const;
-    
+
     /*! \brief Get the assigned type.
      *  \return the assigned ff type for the angle. */
-    const FFAngle& GetType() const;
-    
+    const FFAngle &GetType() const;
+
     /*! \brief Assign an FFAngle type.
      *  \param type the FFAngle type to assign. */
-    void SetType(const FFAngle& type);
-    
-    bool HasType() const;
-    
+    void SetType(const FFAngle &type);
+
   private:
     struct Impl;
     std::shared_ptr<Impl> m_data;
-    
-    //! The molecule this angle is assigned to.
-    wMolecule _mol;
-    //! Tag (unstable)
-    uint32_t _tag;
-    //! MM angle type
-    FFAngle _type;
-    //! \brief Atoms which make up the angle.
-    AngleAtoms _atms;
   };
-  
-  std::ostream& operator<<(std::ostream& os, const Angle& ang);
-}
+} // namespace indigox
 
-#endif  /* INDIGOX_CLASSES_ANGLE_HPP */
+#endif /* INDIGOX_CLASSES_ANGLE_HPP */
--- src/classes/angle.cpp
+++ src/classes/angle.cpp
@@ -1,210 +1,300 @@
-#include <sstream>
-
 #include <indigox/classes/angle.hpp>
 #include <indigox/classes/atom.hpp>
 #include <indigox/classes/forcefield.hpp>
 #include <indigox/classes/molecule.hpp>
-#include <indigox/utils/counter.hpp>
-#include <indigox/utils/serialise.hpp>
-
 #include <indigox/utils/doctest_proxy.hpp>
-#include <indigox/test/angle_test.hpp>
-#include <indigox/test/forcefield_test.hpp>
+
+#include <array>
+#include <memory>
 
 namespace indigox {
   test_suite_open("Angle");
-  
-  // Serialisation of Angle
+
+  // =======================================================================
+  // == IMPLEMENTATION =====================================================
+  // =======================================================================
+
+  struct Angle::Impl {
+    AngleAtoms atoms;
+    Molecule molecule;
+    int64_t tag;
+    int64_t unique_id;
+    FFAngle forcefield_type;
+
+    template <typename Archive>
+    void serialise(Archive &archive, const uint32_t);
+  };
+
+  // =======================================================================
+  // == SERIALISATION ======================================================
+  // =======================================================================
+
   template <typename Archive>
-  void Angle::save(Archive &archive, const uint32_t) const {
-    std::vector<sAtom> atoms;
-    for (wAtom at : _atms) atoms.emplace_back(at.lock());
-    archive(INDIGOX_SERIAL_NVP("molecule", _mol.lock()),
-            INDIGOX_SERIAL_NVP("atoms", atoms),
-            INDIGOX_SERIAL_NVP("tag", _tag),
-            INDIGOX_SERIAL_NVP("type", _type));
+  void Angle::Impl::serialise(Archive &archive, const uint32_t) {
+    archive(INDIGOX_SERIAL_NVP("atoms", atoms),
+            INDIGOX_SERIAL_NVP("molecule", molecule),
+            INDIGOX_SERIAL_NVP("tag", tag),
+            INDIGOX_SERIAL_NVP("unique_id", unique_id),
+            INDIGOX_SERIAL_NVP("type", forcefield_type));
   }
-  
+
   template <typename Archive>
-  void Angle::load_and_construct(Archive &archive,
-                                 cereal::construct<Angle> &construct,
-                                 const uint32_t) {
-    sMolecule m;
-    std::vector<sAtom> atoms;
-    archive(INDIGOX_SERIAL_NVP("molecule", m),
-            INDIGOX_SERIAL_NVP("atoms", atoms));
-    
-    construct(*atoms[0], *atoms[1], *atoms[2], *m);
-    
-    archive(INDIGOX_SERIAL_NVP("tag", construct->_tag),
-            INDIGOX_SERIAL_NVP("type", construct->_type));
+  void Angle::serialise(Archive &archive, const uint32_t) {
+    archive(INDIGOX_SERIAL_NVP("data", m_data));
   }
-  INDIGOX_SERIALISE_CONSTRUCT(Angle);
-  
-/*    DOCTEST_TEST_CASE_TEMPLATE_DEFINE("Angle serialisation", T, ixangle_serial) {
-      using In = typename T::t1;
-      using Out = typename cereal::traits::detail::get_output_from_input<In>::type;
-      FFAngle ffang = test::CreateGenericTestFFAngle().imp;
-      test::AngleTestFixture fixture;
-      Angle saved = fixture.ang.imp;
-  
-      saved->SetTag(23);
-      saved->SetType(ffang);
-  
-      std::ostringstream os;
-      {
-        Out oar(os);
-        check_nothrow(oar(saved, saved->GetAtoms(), saved->GetMolecule(),
-                          saved->GetType()));
+  INDIGOX_SERIALISE(Angle);
+
+  // =======================================================================
+  // == OPERATORS ==========================================================
+  // =======================================================================
+
+  bool Angle::operator==(const Angle &ang) const {
+    if (ang.m_data == m_data) {
+      return true;
+    } else {
+      return false;
+    }
+
+    // Do a more indepth comparison if they're not the exact same angle
+    if (ang.m_data->forcefield_type != m_data->forcefield_type) {
+      return false;
+    }
+    if (ang.m_data->atoms[1] != m_data->atoms[1]) {
+      return false;
+    }
+
+    bool fwd_comp = (ang.m_data->atoms[0] == m_data->atoms[0]) &&
+                    (ang.m_data->atoms[2] == m_data->atoms[2]);
+    bool bwd_comp = (ang.m_data->atoms[2] == m_data->atoms[0]) &&
+                    (ang.m_data->atoms[0] == m_data->atoms[2]);
+    return (fwd_comp || bwd_comp);
+  }
+
+  bool Angle::operator<(const Angle &ang) const {
+    return m_data->unique_id < ang.m_data->unique_id;
+  }
+  bool Angle::operator>(const Angle &ang) const {
+    return m_data->unique_id > ang.m_data->unique_id;
+  }
+
+  // =======================================================================
+  // == CONSTRUCTION =======================================================
+  // =======================================================================
+
+  Angle::Angle(const Atom &a, const Atom &b, const Atom &c, const Molecule &m)
+      : m_data(std::make_shared<Impl>()) {
+    m_data->atoms[0] = a;
+    m_data->atoms[1] = b;
+    m_data->atoms[2] = c;
+    m_data->molecule = m;
+  }
+
+  // =======================================================================
+  // == STATE CHECKING =====================================================
+  // =======================================================================
+
+  bool Angle::HasType() const {
+    return bool(*this) ? bool(m_data->forcefield_type) : false;
+  }
+
+  // =======================================================================
+  // == STATE GETTING ======================================================
+  // =======================================================================
+
+  int64_t Angle::GetTag() const {
+    return *this ? m_data->tag
+                 : throw std::runtime_error(
+                       "Attempting to access data from invalid angle instance");
+  }
+
+  int64_t Angle::GetID() const {
+    return *this ? m_data->unique_id
+                 : throw std::runtime_error(
+                       "Attempting to access data from invalid angle instance");
+  }
+
+  const Molecule &Angle::GetMolecule() const {
+    return *this ? m_data->molecule
+                 : throw std::runtime_error(
+                       "Attempting to access data from invalid angle instance");
+  }
+
+  const Angle::AngleAtoms &Angle::GetAtoms() const {
+    return *this ? m_data->atoms
+                 : throw std::runtime_error(
+                       "Attempting to access data from invalid angle instance");
+  }
+
+  const FFAngle &Angle::GetType() const {
+    return *this ? m_data->forcefield_type
+                 : throw std::runtime_error(
+                       "Attempting to access data from invalid angle instance");
+  }
+
+  int64_t Angle::GetIndex() const {
+    if (!(*this)) {
+      throw std::runtime_error(
+          "Attempting to access data from invalid angle instance");
+    }
+    if (!m_data->molecule) {
+      return -1;
+    }
+    auto mol_angles = m_data->molecule.GetAngles();
+    auto idx = std::find(mol_angles.begin(), mol_angles.end(), *this);
+    return (idx == mol_angles.end()) ? -1
+                                     : std::distance(mol_angles.begin(), idx);
+  }
+
+  // =======================================================================
+  // == STATE SETTING ======================================================
+  // =======================================================================
+
+  void Angle::SetType(const FFAngle &type) {
+    if (!(*this)) {
+      throw std::runtime_error(
+          "Attempting to access data from invalid angle instance");
+    }
+    if (m_data->molecule) {
+      if (!m_data->molecule.HasForcefield()) {
+        m_data->molecule.SetForcefield(type.GetForcefield());
+      } else if (m_data->molecule.GetForcefield() != type.GetForcefield()) {
+        throw std::runtime_error(
+            "Provided angle type does not match molecule's forcefield");
       }
-  
-      Angle loaded;
-      test::TestAngle::Atoms atoms;
-      Molecule mol;
-      FFAngle loaded_type;
-      std::istringstream is(os.str());
-      {
-        In iar(is);
-        check_nothrow(iar(loaded, atoms, mol, loaded_type));
-      }
-      fixture.ang.imp = loaded;
-      check_false(fixture.ang.get_atms()[0].expired());
-      check_false(fixture.ang.get_atms()[1].expired());
-      check_false(fixture.ang.get_atms()[2].expired());
-      check_false(fixture.ang.get_mol().expired());
-      check_false(fixture.ang.get_type().use_count() == 0);
-      check_eq(saved->GetTag(), loaded->GetTag());
-      check_eq(atoms.first->GetTag(), saved->GetAtoms().first->GetTag());
-      check_eq(atoms.second->GetTag(), saved->GetAtoms().second->GetTag());
-      check_eq(atoms.third->GetTag(), saved->GetAtoms().third->GetTag());
-      check_eq(atoms, loaded->GetAtoms());
     }
-    DOCTEST_TEST_CASE_TEMPLATE_INSTANTIATE(ixangle_serial, ixserial<IXAngle>);
- */
-  
-  Angle::Angle(Atom& a, Atom& b, Atom& c, Molecule& m)
-  : utils::IXCountableObject<Angle>(), _mol(m.weak_from_this()), _tag(0),
-  _atms({a.weak_from_this(), b.weak_from_this(), c.weak_from_this()}) { }
-  
-/*    test_case_fixture(test::AngleTestFixture, "IXAngle construction") {
-      check_nothrow(test::TestAngle tang(a,b,c,mol));
-      test::TestAngle tang(a,b,c,mol);
-      check_eq(mol, tang.get_mol().lock());
-      check_eq(0, tang.get_tag());
-      check_eq(a, tang.get_atms()[0].lock());
-      check_eq(b, tang.get_atms()[1].lock());
-      check_eq(c, tang.get_atms()[2].lock());
-      check_eq(FFAngle(), tang.get_type());
-  
-      // Check unique IDs correctly update
-      test::TestAngle ang1(a,b,c,mol);
-      test::TestAngle ang2(a,b,c,mol);
-      check_ne(ang1.GetUniqueID(), ang2.GetUniqueID());
-      check_eq(ang1.GetUniqueID() + 1, ang2.GetUniqueID());
-    }
- */
-  
-  size_t Angle::GetIndex() const {
-    if (_mol.expired()) return GetTag();
-    Molecule& mol = *_mol.lock();
-    auto be = mol.GetAngleIters();
-    auto pos = std::find(be.first, be.second, shared_from_this());
-    if (pos == be.second) return GetTag();
-    return std::distance(be.first, pos);
+    m_data->forcefield_type = type;
   }
-  
-  
-/*    test_case_fixture(test::AngleTestFixture, "IXAngle getting and setting") {
-      FFAngle ffang = test::CreateGenericTestFFAngle().imp;
-  
-      // Check no throwing
-      check_nothrow(ang.GetTag());
-      check_nothrow(ang.SetTag(72));
-      check_nothrow(ang.GetMolecule());
-      check_nothrow(ang.GetAtoms());
-      check_nothrow(ang.SwapOrder());
-      check_nothrow(ang.NumAtoms());
-      check_nothrow(ang.GetType());
-      check_nothrow(ang.SetType(ffang));
-  
-      // Check correctness of gets
-      check_eq(72, ang.GetTag());
-      check_eq(72, ang.get_tag());
-      check_eq(mol, ang.GetMolecule());
-      check_eq(mol, ang.get_mol().lock());
-      check_eq(stdx::make_triple(c, b, a), ang.GetAtoms());
-      check_eq(ang.GetTag(), ang.GetIndex());
-      check_eq(ffang, ang.GetType());
-  
-      // Check correct no owning of molecule
-      mol.reset();
-      check(ang.get_mol().expired());
-      check_eq(Molecule(), ang.GetMolecule());
-  
-      // Check get index returns tag when molecule dead
-      check_eq(ang.GetTag(), ang.GetIndex());
-  
-      // Check num atoms doesn't depend on being active
-      check_eq(3, ang.NumAtoms());
-      a.reset(); b.reset(); c.reset();
-      check_eq(3, ang.NumAtoms());
+
+  // =======================================================================
+  // == OUTPUTTING =========================================================
+  // =======================================================================
+
+  std::ostream &operator<<(std::ostream &os, const Angle &ang) {
+    if (ang) {
+      const Angle::AngleAtoms &atoms = ang.GetAtoms();
+      os << "Angle(" << atoms[0].GetIndex() + 1 << ", "
+         << atoms[1].GetIndex() + 1 << ", " << atoms[2].GetIndex() + 1 << ")";
     }
- */
-  
-  std::string Angle::ToString() const {
-    std::stringstream ss;
-    
-    Atom& a = *_atms[0].lock();
-    Atom& b = *_atms[1].lock();
-    Atom& c = *_atms[2].lock();
-    ss << "Angle(" << a.ToString() << ", " << b.ToString() << ", "
-       << c.ToString() << ")";
-    return ss.str();
-  }
-  
-  std::ostream& operator<<(std::ostream& os, const Angle& ang) {
-    auto s = ang.GetAtoms();
-    os << "Angle(" << s.first.GetIndex() + 1 << ", " << s.second.GetIndex() + 1
-       << ", " << s.third.GetIndex() + 1<< ")";
     return os;
   }
-  
-/*    test_case_fixture(test::AngleTestFixture, "IXAngle printing methods") {
-      // Check ordering is correct
-      std::stringstream ss; ss << ang.imp;
-      check_eq("Angle(Atom(0, C), Atom(1, O), Atom(2, N))", ang.ToString());
-      check_eq("Angle(0, 1, 2)", ss.str());
-      ang.SwapOrder();
-      ss.str(""); ss << ang.imp;
-      check_eq("Angle(Atom(2, N), Atom(1, O), Atom(0, C))", ang.ToString());
-      check_eq("Angle(2, 1, 0)", ss.str());
-  
-      // Check having bad atoms is handled correctly
-      a.reset();
-      ss.str(""); ss << ang.imp;
-      check_eq("Angle(MALFORMED)", ang.ToString());
-      check_eq("Angle(2, 1, )", ss.str());
-  
-      // Check empty angle to ostream does nothing
-      ss.str("");
-      check_nothrow(ss << Angle());
-      check_eq("", ss.str());
-    }
- */
-  
-  void Angle::SetType(const FFAngle& type) {
-    if (!GetMolecule().HasForcefield())
-      GetMolecule().SetForcefield(type.GetForcefield());
-    if (type.GetForcefield() != GetMolecule().GetForcefield())
-      throw std::runtime_error("Angle type is not from molecule's forcefield");
-    _type = type;
-  }
-  
-  void Angle::Clear() {
-//    _mol.reset();
-//    _type = FFAngle();
-//    _atms.fill(wAtom());
-  }
-  
+
+  /*    DOCTEST_TEST_CASE_TEMPLATE_DEFINE("Angle serialisation", T,
+   ixangle_serial) { using In = typename T::t1; using Out = typename
+   cereal::traits::detail::get_output_from_input<In>::type; FFAngle ffang =
+   test::CreateGenericTestFFAngle().imp; test::AngleTestFixture fixture; Angle
+   saved = fixture.ang.imp;
+
+   saved->SetTag(23);
+   saved->SetType(ffang);
+
+   std::ostringstream os;
+   {
+   Out oar(os);
+   check_nothrow(oar(saved, saved->GetAtoms(), saved->GetMolecule(),
+   saved->GetType()));
+   }
+
+   Angle loaded;
+   test::TestAngle::Atoms atoms;
+   Molecule mol;
+   FFAngle loaded_type;
+   std::istringstream is(os.str());
+   {
+   In iar(is);
+   check_nothrow(iar(loaded, atoms, mol, loaded_type));
+   }
+   fixture.ang.imp = loaded;
+   check_false(fixture.ang.get_atms()[0].expired());
+   check_false(fixture.ang.get_atms()[1].expired());
+   check_false(fixture.ang.get_atms()[2].expired());
+   check_false(fixture.ang.get_mol().expired());
+   check_false(fixture.ang.get_type().use_count() == 0);
+   check_eq(saved->GetTag(), loaded->GetTag());
+   check_eq(atoms.first->GetTag(), saved->GetAtoms().first->GetTag());
+   check_eq(atoms.second->GetTag(), saved->GetAtoms().second->GetTag());
+   check_eq(atoms.third->GetTag(), saved->GetAtoms().third->GetTag());
+   check_eq(atoms, loaded->GetAtoms());
+   }
+   DOCTEST_TEST_CASE_TEMPLATE_INSTANTIATE(ixangle_serial, ixserial<IXAngle>);
+   */
+
+  /*    test_case_fixture(test::AngleTestFixture, "IXAngle construction") {
+        check_nothrow(test::TestAngle tang(a,b,c,mol));
+        test::TestAngle tang(a,b,c,mol);
+        check_eq(mol, tang.get_mol().lock());
+        check_eq(0, tang.get_tag());
+        check_eq(a, tang.get_atms()[0].lock());
+        check_eq(b, tang.get_atms()[1].lock());
+        check_eq(c, tang.get_atms()[2].lock());
+        check_eq(FFAngle(), tang.get_type());
+
+        // Check unique IDs correctly update
+        test::TestAngle ang1(a,b,c,mol);
+        test::TestAngle ang2(a,b,c,mol);
+        check_ne(ang1.GetUniqueID(), ang2.GetUniqueID());
+        check_eq(ang1.GetUniqueID() + 1, ang2.GetUniqueID());
+      }
+   */
+
+  /*    test_case_fixture(test::AngleTestFixture, "IXAngle getting and setting")
+     { FFAngle ffang = test::CreateGenericTestFFAngle().imp;
+
+        // Check no throwing
+        check_nothrow(ang.GetTag());
+        check_nothrow(ang.SetTag(72));
+        check_nothrow(ang.GetMolecule());
+        check_nothrow(ang.GetAtoms());
+        check_nothrow(ang.SwapOrder());
+        check_nothrow(ang.NumAtoms());
+        check_nothrow(ang.GetType());
+        check_nothrow(ang.SetType(ffang));
+
+        // Check correctness of gets
+        check_eq(72, ang.GetTag());
+        check_eq(72, ang.get_tag());
+        check_eq(mol, ang.GetMolecule());
+        check_eq(mol, ang.get_mol().lock());
+        check_eq(stdx::make_triple(c, b, a), ang.GetAtoms());
+        check_eq(ang.GetTag(), ang.GetIndex());
+        check_eq(ffang, ang.GetType());
+
+        // Check correct no owning of molecule
+        mol.reset();
+        check(ang.get_mol().expired());
+        check_eq(Molecule(), ang.GetMolecule());
+
+        // Check get index returns tag when molecule dead
+        check_eq(ang.GetTag(), ang.GetIndex());
+
+        // Check num atoms doesn't depend on being active
+        check_eq(3, ang.NumAtoms());
+        a.reset(); b.reset(); c.reset();
+        check_eq(3, ang.NumAtoms());
+      }
+   */
+
+  /*    test_case_fixture(test::AngleTestFixture, "IXAngle printing methods") {
+        // Check ordering is correct
+        std::stringstream ss; ss << ang.imp;
+        check_eq("Angle(Atom(0, C), Atom(1, O), Atom(2, N))", ang.ToString());
+        check_eq("Angle(0, 1, 2)", ss.str());
+        ang.SwapOrder();
+        ss.str(""); ss << ang.imp;
+        check_eq("Angle(Atom(2, N), Atom(1, O), Atom(0, C))", ang.ToString());
+        check_eq("Angle(2, 1, 0)", ss.str());
+
+        // Check having bad atoms is handled correctly
+        a.reset();
+        ss.str(""); ss << ang.imp;
+        check_eq("Angle(MALFORMED)", ang.ToString());
+        check_eq("Angle(2, 1, )", ss.str());
+
+        // Check empty angle to ostream does nothing
+        ss.str("");
+        check_nothrow(ss << Angle());
+        check_eq("", ss.str());
+      }
+   */
+
   test_suite_close();
-}
+} // namespace indigox
