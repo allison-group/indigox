ed58ac8d24e6574055f72791c07f51bf5d119c4a
iwelsh47 <8754457+iwelsh47@users.noreply.github.com>
2019-02-15 10:41:39 +1300

    Moved CherryPicker example into a sub directory

--- examples/CherryPicker/cherrypicker_example.py
+++ examples/CherryPicker/cherrypicker_example.py
@@ -7,24 +7,23 @@ from pathlib import Path
 ff = ix.GenerateGROMOS54A7()
 
 # Generate and save the Athenaeums used in the paper
-# On our system this takes approximately six minutes to execute and uses a maximum of 8GB of memory.
-# The saved files are 456kB and 779.7 MB respectively
+# On our system this takes approximately 6.5 minutes to execute and uses a maximum of 3.5GB of memory.
+# The saved files are 463 kB and 684.2 MB respectively.
 def LoadAndSaveAthenaeums():
+    settings = ix.Athenaeum.Settings
     man_ath = ix.Athenaeum(ff)
-    man_ath.SetSelfConsistent()
-
+    man_ath.SetBool(settings.SelfConsistent)
     auto_ath = ix.Athenaeum(ff, 1)
 
     # Need to set a larger than default limit as some of the amino acid molecules are large
-    ix.Athenaeum.Settings.AtomLimit = 60
+    auto_ath.SetInt(settings.MoleculeSizeLimit, 60)
     
     # Load all the molecules
     for aa in Path("SourceMolecules").glob("*.frag"):
         mol, fragments = ix.LoadFragmentFile(aa, ff)
         mol.SetName(aa.stem)
         # add the fragments to the manual Athenaeum
-        for frag in fragments:
-            man_ath.AddFragment(mol, frag)
+        for frag in fragments: man_ath.AddFragment(frag)
         # add the molecule to the automatic Athenaeum
         auto_ath.AddAllFragments(mol)
 
@@ -35,6 +34,7 @@ def LoadAndSaveAthenaeums():
 # Load athenaeums and run the cherrypicker algorithm on the 3 molecules used in the paper
 # On our system, loading the Athenaeums takes about 12 seconds, then running Loading, Running CherryPicker, and Saving the test molecules takes about 5 seconds.
 def RunCherryPicker():
+    settings = ix.algorithm.CherryPicker.Settings
     # Load the athenaeums
     man_ath = ix.LoadAthenaeum("ManualAthenaeum.ath")
     auto_ath = ix.LoadAthenaeum("AutomaticAthenaeum.ath")  # Is a large file so may take a few seconds to load
@@ -45,7 +45,8 @@ def RunCherryPicker():
     cherrypicker.AddAthenaeum(auto_ath)
     
     # Set the CherryPicker options we want
-    ix.algorithm.CherryPicker.Settings.MinimumFragmentSize = 2
+    cherrypicker.SetInt(settings.MinimumFragmentSize, 2)
+    cherrypicker.SetInt(settings.MaximumFragmentSize, 20)
 
     # Load each of the test molecules and run cherrypicker
     for test in Path("TestMolecules").glob("*.pdb"):
@@ -53,7 +54,7 @@ def RunCherryPicker():
         parameterised = cherrypicker.ParameteriseMolecule(mol)
 
         # save the parameterisation in ITP format
-        ix.SaveITPFile(Path("{}.itp".format(test.stem)), mol, parameterised)
+        ix.SaveITPFile(test.with_suffix(".itp"), mol, parameterised)
 
 if __name__ == "__main__":
     LoadAndSaveAthenaeums()
